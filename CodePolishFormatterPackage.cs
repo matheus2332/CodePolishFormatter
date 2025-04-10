54f0571832f27533ec55749eb89bac15951b7b7e
matheus siqueira <matheus2332@hotmail.com>
2025-04-10 12:25:54 +0100

    Initial commit with CodePolishFormatter code

---
+++ CodePolishFormatterPackage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace CodePolishFormatter
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(CodePolishFormatterPackage.PackageGuidString)]
+    public sealed class CodePolishFormatterPackage : AsyncPackage
+    {
+        public const string PackageGuidString = "246f71ab-51d0-4b7d-b04a-c364b0702930";
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await base.InitializeAsync(cancellationToken, progress);
+
+            // Register the Format Code command
+            await FormatCodeCommand.InitializeAsync(this);
+        }
+    }
+}
---
+++ FormatCodeCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel.Design;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace CodePolishFormatter
+{
+    internal sealed class FormatCodeCommand
+    {
+        public const int CommandId = 0x0100;
+        public static readonly Guid CommandSet = new Guid("f78e9b26-2f8a-4ac9-bc01-ff562f7b784b"); // GUID do comando
+        private readonly AsyncPackage _package;
+        private OleMenuCommand _menuCommand;
+
+        private FormatCodeCommand(AsyncPackage package, IMenuCommandService commandService)
+        {
+            _package = package ?? throw new ArgumentNullException(nameof(package));
+
+            // Criando o comando e registrando no menu
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            _menuCommand = new OleMenuCommand(Execute, menuCommandID);
+            commandService.AddCommand(_menuCommand);
+        }
+
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Obtendo o serviço de comando de forma assíncrona
+            var commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as IMenuCommandService;
+
+            if (commandService != null)
+            {
+                // Inicializando o comando de formatação
+                new FormatCodeCommand(package, commandService);
+            }
+            else
+            {
+                throw new InvalidOperationException("Não foi possível obter o IMenuCommandService.");
+            }
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            // Aqui você pode adicionar a lógica de formatação de código
+            VsShellUtilities.ShowMessageBox(
+                _package,
+                "Code formatted (simulated)!",
+                "AutoCodeFormatter",
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
